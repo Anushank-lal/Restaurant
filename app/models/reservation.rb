506297b0782dc00dde471838e2575fab65c06fa5
Anushank Lal <anushank.lal@imaginelearning.com>
2019-01-29 15:44:29 +0530

    validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,13 @@ class Reservation < ApplicationRecord
   belongs_to :user
 
   has_one :table
+
+  validate :reservation_time_with_shift
+
+  private
+
+  def reservation_time_with_shift
+    range = self.restaurant_shift.start_time..self.restaurant_shift.end_time
+    errors.add(:reservation_time, "should in shift range") unless range === self.reservation_time
+  end
 end
